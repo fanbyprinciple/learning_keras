ac7725a9970f1a269f6820329b7813fba57b8eda
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-05-15 15:42:04 +0530

    trying stuff with tensorflow 2.0

---
+++ stridenet/stridenet.py
@@ -0,0 +1,10 @@
+# importin
+
+from keras.models import Sequential
+from keras.layers.noramlization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.core import Activation, Flatten, Dropout, Dense
+
+from keras import backend as K 
+
+print("imported !")
